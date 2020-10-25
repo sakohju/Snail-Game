e5949e482807faacefb9c6a5b06de200d50d24f7
Jade <jhmarmo@charter.net>
2020-10-24 21:45:19 -0700

    implemented audio

--- Snail Game/Assets/Scripts/Player.cs
+++ Snail Game/Assets/Scripts/Player.cs
@@ -10,6 +10,9 @@ public class Player : MonoBehaviour
     public float rotateSpeed;
 
     private bool inShell; //boolean true if retracted in shell, false if not
+    private AudioSource retractsound; //shell retract sound effect
+
+    private Animator animator;
 
     private CharacterController controller;
 
@@ -17,25 +20,60 @@ public class Player : MonoBehaviour
     void Start()
     {
         controller = this.GetComponent<CharacterController>();
-
+        animator = this.GetComponent<Animator>();
+        retractsound = this.GetComponent<AudioSource>();
+        inShell = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if(Input.GetKey(KeyCode.Escape)){
             //QUIT GAME
             Application.Quit();
         }
 
-        //MOVEMENT
-        // Rotate around y - axis
-        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        if(Input.GetKeyDown(KeyCode.LeftShift)){
+            //SHELL RETRACTION
+            if(!inShell){
+                //go into shell
+                animator.SetBool("Retract", true);
+                animator.SetBool("Crawl", false);
+                animator.SetBool("Idle", false);
+            }
+            else{
+                //come out of shell
+                animator.SetBool("Retract", false);
+                animator.SetBool("Idle", true);
+            }
+            inShell = !inShell;
+            retractsound.Play();
 
-        // Move forward / backward
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        float curSpeed = speed * Input.GetAxis("Vertical");
-        controller.SimpleMove(forward * curSpeed);
+        }
+
+        if (!inShell)
+        {
+            //MOVEMENT
+            // Rotate around y - axis
+            transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+            // Move forward / backward
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            float curSpeed = speed * Input.GetAxis("Vertical");
+            controller.SimpleMove(forward * curSpeed);
+
+            //Animations
+            if(System.Math.Abs(curSpeed) > Mathf.Epsilon)
+            {
+                animator.SetBool("Crawl", true);
+                animator.SetBool("Idle", false);
+            }
+            else{
+                animator.SetBool("Crawl", false);
+                animator.SetBool("Idle", true);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
