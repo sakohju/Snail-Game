0ff01b9a377d252772ca6d408d2ba018ee3067a1
Jade <jhmarmo@charter.net>
2020-10-24 20:38:21 -0700

    movement and camera works!

--- Snail Game/Assets/Scripts/Player.cs
+++ Snail Game/Assets/Scripts/Player.cs
@@ -6,17 +6,36 @@ public class Player : MonoBehaviour
 {
 
     public int hp; //hp stat
+    public float speed; //speed 
+    public float rotateSpeed;
+
+    private bool inShell; //boolean true if retracted in shell, false if not
+
+    private CharacterController controller;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = this.GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Escape)){
+            //QUIT GAME
+            Application.Quit();
+        }
+
+        //MOVEMENT
+        // Rotate around y - axis
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        // Move forward / backward
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
     }
 
     private void OnTriggerEnter(Collider other)
